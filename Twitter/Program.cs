ea87f67f2e63b17e0204478a33e008c57fab41fc
mouaa <mouaa@DESKTOP-NT2AJPK>
2023-09-22 16:35:11 -0400

    Add project files.

---
+++ Twitter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your post: ");
+            string post = Console.ReadLine();
+            if (post.Length > 140) 
+            {
+                Console.WriteLine("Rejected");
+                Console.ReadLine();
+            } else if (post.Length <= 140) 
+            {
+                Console.WriteLine("Posted");
+                Console.ReadLine();
+            }
+        }
+    }
+}
